f9dbe0f3095ae2e5514ec5fd37eb793dc289dbbc
suti290502 <yennhpc@gmail.com>
2024-04-12 09:51:48 +0700

    push

--- TrainingFPTCo/obj/Debug/net8.0/TrainingFPTCo.AssemblyInfo.cs
+++ TrainingFPTCo/obj/Debug/net8.0/TrainingFPTCo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee27c4206e56809837dddbee13765a6971d3faac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
